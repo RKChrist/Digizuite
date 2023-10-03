9e52eb861c162c438401998a00e15c5c43b6065c
RKChrist <roo12ney@gmail.com>
2023-10-03 11:27:34 +0200

    Updated API

---
+++ API/API/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using API.RabbitMQ;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Data.Common;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+
+        private IModel channel;
+        private IRabbitConnection _connection; 
+
+        public HomeController(IRabbitConnection connection)
+        {
+            _connection = connection;
+        }
+
+        [HttpGet]
+        public IActionResult Index(HttpRequestMessage request)
+        {
+            Response.StatusCode = StatusCodes.Status418ImATeapot;
+            return View("Im a teapot");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendToRabbit(IFormFile File, int? wishedWidth, int? wishedHeight)
+        {
+            string exchange = "e_files";
+            string exchangeType = ExchangeType.Headers;
+            string key = "x-filetype";
+            string queueName = "pdf";
+            try
+            {
+                MemoryStream ms = new MemoryStream(new byte[File.Length]);
+                await File.CopyToAsync(ms);
+
+                await File.CopyToAsync(ms);
+                Dictionary<string, object> headers = new Dictionary<string, object>();
+                headers.Add("x-match", "any");
+                headers.Add(key, File.ContentType);
+                if (File.ContentType.Contains("image"))
+                {
+                    using var image = SixLabors.ImageSharp.Image.Load(File.OpenReadStream());
+                    headers.Add("Width", image.Width);
+                    headers.Add("Height", image.Height);
+                    queueName = "Images";
+                }
+                _connection.SendUsingHeaders("Images", exchange, exchangeType, headers, ms.ToArray());
+            }
+            catch(Exception ex)
+            {
+                
+            }
+
+
+            return Ok(File.FileName + ": Hello :" + File.ContentType + ": Hello : " + File.ContentDisposition);
+
+        }
+    }
+}
---
+++ API/API/RabbitMQ/RabbitConnection.cs
@@ -0,0 +1,136 @@
+﻿using RabbitMQ.Client;
+using System.Reflection;
+using System.Text;
+
+namespace API.RabbitMQ
+{
+    public class RabbitConnection : IRabbitConnection
+    {
+        // https://stackoverflow.com/questions/15033848/how-can-a-rabbitmq-client-tell-when-it-loses-connection-to-the-server
+        private string host = "localhost";
+        private int port = 6000;
+        private ConnectionFactory _factory;
+        private IConnection connection;
+
+        private bool isConnected = false;
+        private static IModel channel = null;
+        private Dictionary<string, IModel> channelDict = new Dictionary<string, IModel>();
+        private readonly ILogger<RabbitConnection> _logger;
+
+        public RabbitConnection(ILogger<RabbitConnection> logger)
+        {
+            _logger = logger;
+            _factory = new ConnectionFactory { HostName = host, Port = port, RequestedHeartbeat = TimeSpan.FromSeconds(16) };
+
+        }
+        private IConnection CreateConnection()
+        {
+            IConnection createdConnection = null;
+            while (!isConnected)
+            {
+                try
+                {
+                    // https://www.rabbitmq.com/heartbeats.html <-- docs says values between 5 and 20 seconds are optimal
+                    createdConnection = _factory.CreateConnection();
+                    isConnected = true;
+                    _logger.LogInformation("createConnection(): Connected to RabbitMQ at " + host + ":" + port);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogInformation("createConnection(): Connection attempt to RabbitMQ at " + host + ":" + port + " failed");
+                    _logger.LogInformation(e.Message);
+                }
+                Thread.Sleep(1500);
+            }
+            return createdConnection;
+        }
+        public IModel CreateChannel(string exchange, string exchangeType)
+        {
+            //TODO: make this use the input parameter exchangeType
+            // "direct", "fanout", "headers", "topic"
+            if (isConnected)
+            {
+                _logger.LogInformation("RabbitConnection.createChannel(string exchange, string exchangeType): is connected");
+                channel = connection.CreateModel();
+                channel.ExchangeDeclare(exchange: exchange, type: exchangeType);
+                return channel;
+            }
+            else
+            {
+                _logger.LogInformation("createChannel(): no connection to RabbitMQ");
+                throw new Exception("no connection to RabbitMQ");
+            }
+        }
+
+        // exchange, type
+
+        private void OnConnectionLost(object? sender, ShutdownEventArgs reason)
+        {
+            isConnected = false;
+            connection = CreateConnection();
+        }
+
+        public IModel GetChannel(string exchange, string exchangeType)
+        {
+            string channelKey = CreateChannelKey(exchange, exchangeType);
+
+
+            if (connection is null)
+            {
+                connection = CreateConnection();
+                connection.ConnectionShutdown += OnConnectionLost;
+            }
+
+            if (channelDict.ContainsKey(channelKey))
+            {
+                _logger.LogInformation("getChannel(): channel exists");
+                return channelDict[channelKey];
+            }
+
+            else
+            {
+                IModel createdChannel = CreateChannel(exchange, exchangeType);
+                channelDict.Add(channelKey, createdChannel);
+                _logger.LogInformation("getChannel(): channel created");
+                return channelDict[channelKey];
+            }
+        }
+
+        public bool SendExchange(string exchange, string exchangeType, string routingKey, string message)
+        {
+            bool result = false;
+            string channelKey = CreateChannelKey(exchange, exchangeType);
+            IModel channel = GetChannel(exchange, exchangeType);
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: exchange,
+                                    routingKey: routingKey,
+                                    basicProperties: null,
+                                    body: body);
+
+            return result;
+        }
+
+        public bool SendUsingHeaders(string queueName, string exchangeName, string exchangeType, Dictionary<string, object> Headers, byte[]? message)
+        {
+            string channelKey = CreateChannelKey(exchangeName, exchangeType);
+            IModel channel = GetChannel(exchangeName, exchangeType);
+            //Properties
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+            channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: "", Headers);
+
+            channel.BasicPublish(exchange: exchangeName,
+                                 routingKey: "",
+                                 basicProperties: properties,
+                                 body: message);
+            return true;
+
+        }
+
+        private string CreateChannelKey(string exchange, string exchangeType)
+        {
+            return exchange + "+" + exchangeType;
+        }
+    }
+}
