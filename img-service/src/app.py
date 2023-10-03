bd5269a1c398e7ef09c8a2b8454db7ab86b4169a
michael <michael@rulle.xyz>
2023-10-03 19:55:16 +0200

    almost done with image-resizer image - need to extract headers, then end-to-end test

--- img-service/src/app.py
+++ img-service/src/app.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 import pika
-import io
+import io, os
 from image_resizer import ImageResizer
+
+RABBIT_MQ_HOST = os.environ['RABBIT_MQ_HOST']
+RABBIT_MQ_PORT = os.environ['RABBIT_MQ_PORT']
 
 resizer = ImageResizer()
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost', port=6000))
+    pika.ConnectionParameters(host=RABBIT_MQ_HOST, port=RABBIT_MQ_PORT))
 channel = connection.channel()
 
 queue_name = 'q_images'
@@ -19,6 +22,7 @@ channel.queue_bind(exchange='e_files', queue='q_images')
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
+
     resized_image = resizer.resize_from_bytes(body, 128, 128)
     
     print(type(body))
