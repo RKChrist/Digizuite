c1921e331b3b304af10f0c0e4fd3d0f0de0d9178
michael <michael@rulle.xyz>
2023-10-04 09:25:08 +0200

    added env variables to image-service

--- img-service/src/app.py
+++ img-service/src/app.py
@@ -3,8 +3,10 @@ import pika
 import io, os
 from image_resizer import ImageResizer
 
-RABBIT_MQ_HOST = os.environ['RABBIT_MQ_HOST']
-RABBIT_MQ_PORT = os.environ['RABBIT_MQ_PORT']
+import pprint
+
+RABBIT_MQ_HOST = os.environ.get('RabbitMQServerHost', 'localhost')
+RABBIT_MQ_PORT = os.environ.get('RabbitMQServerPort', '5672')
 
 resizer = ImageResizer()
 connection = pika.BlockingConnection(
@@ -22,13 +24,16 @@ channel.queue_bind(exchange='e_files', queue='q_images')
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
-
+    headers = properties.headers
+    print('headers:\n', headers)
     resized_image = resizer.resize_from_bytes(body, 128, 128)
-    
-    print(type(body))
-    print(f" [x] {properties}")
-
+    pprint.pprint(properties)
 channel.basic_consume(
     queue=queue_name, on_message_callback=callback, auto_ack=True)
 
 channel.start_consuming()
+
+## 332a885e79c5   rabbitmq:3.12.3-management-alpine "docker-entrypoint.s…"
+# 0.0.0.0:6000->5672/tcp
+# 0.0.0.0:8080->15672/tcp
+# some-rabbit
--- img-service/src/test.py
+++ img-service/src/test.py
@@ -4,13 +4,15 @@ from image_resizer import ImageResizer
 
 resizer = ImageResizer()
 
+# the point of using argparser was to make type check on parameters, but i didn't quite figure out how to use that option
+
 parser = argparse.ArgumentParser()
 parser.add_argument("path")
 # parser.add_argument("-p", help="path to your image file goes here", action='store_true')
 parser.add_argument("width")
-# parser.add_argument("-x", help="desired witdh goes here", action='store_true')
+# parser.add_argument("-x", help="desired witdh goes here", action='store_true', type=int)
 parser.add_argument("height")
-# parser.add_argument("-y", help="desired witdh goes here", action='store_true')
+# parser.add_argument("-y", help="desired witdh goes here", action='store_true', type=int)
 print("yay")
 args = parser.parse_args()
 
